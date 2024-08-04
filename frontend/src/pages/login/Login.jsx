d18693dfcdc0b2c5e253758875f2139f206ff0a0
Iheb Garsi <ihebgarsi78@gmail.com>
2024-08-04 22:47:35 +0100

    google auth

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
 function Login() {
-  console.log('ee')
+  const handleCallback = (response) => {
+    const user = jwtDecode(response.credential);
+    console.log(user);
+  };
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        "745484482146-nb1euo7dj38rge6cb5c4mre2u3k0vsqc.apps.googleusercontent.com",
+      callback: handleCallback,
+    });
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "marge",
+    });
+  }, []);
   return (
     <div>
-      sss  
+      <div className=" absolute top-[20%]" id="signInDiv"></div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
